e567b11f78e24b5df3b8e06a334a5235462b20c3
Sergio del Cerro <DevBySergio@gmail.com>
2025-03-24 19:01:26 +0100

    SdC_Commit de la primera clase circulo

---
+++ Examen2EV/src/src/SdC_Circulo.java
@@ -0,0 +1,53 @@
+package src;
+
+public class SdC_Circulo {
+
+	private int id;
+	private double Radio;
+	private String Color;
+	
+	public SdC_Circulo(int id, double radio, String color) {
+		super();
+		this.id = id;
+		Radio = radio;
+		Color = color;
+	}
+
+	public SdC_Circulo() {
+		super();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getRadio() {
+		return Radio;
+	}
+
+	public void setRadio(double radio) {
+		Radio = radio;
+	}
+
+	public String getColor() {
+		return Color;
+	}
+
+	public void setColor(String color) {
+		Color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "SdC_Circulo [id=" + id + ", Radio=" + Radio + ", Color=" + Color + "]";
+	}
+	
+	
+	
+	
+	
+}
